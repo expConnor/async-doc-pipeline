cd93e5d042b96e447a18d77fd35e1b36b9201933
Connor R <connorrisse@proton.me>
2026-05-07 17:47:29 +0200

    add service providers

--- api/app/core/providers.py
+++ api/app/core/providers.py
@@ -10,6 +10,10 @@ from app.interfaces.repositories.account import IAccountRepository
 from app.interfaces.repositories.artifact import IArtifactRepository
 from app.interfaces.repositories.document import IDocumentRepository
 from app.interfaces.repositories.job import IJobRepository
+from app.interfaces.services.account import IAccountService
+from app.interfaces.services.artifact import IArtifactService
+from app.interfaces.services.document import IDocumentService
+from app.interfaces.services.job import IJobService
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -46,3 +50,23 @@ def get_messaging_service() -> IMessagingService:
 @lru_cache
 def get_storage_service() -> IStorageService:
     return container.storage_service()
+
+
+@lru_cache
+def get_account_service() -> IAccountService:
+    return container.account_service()
+
+
+@lru_cache
+def get_document_service() -> IDocumentService:
+    return container.document_service()
+
+
+@lru_cache
+def get_job_service() -> IJobService:
+    return container.job_service()
+
+
+@lru_cache
+def get_artifact_service() -> IArtifactService:
+    return container.artifact_service()
