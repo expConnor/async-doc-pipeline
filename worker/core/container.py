850aa8691275e2bd8479dc9e5e6fc182855032f1
Connor R <connorrisse@proton.me>
2026-05-12 14:27:03 +0200

    feat: wire document_repository and messaging_service in worker container

--- worker/core/container.py
+++ worker/core/container.py
@@ -9,11 +9,17 @@ from sqlalchemy.ext.asyncio import (
 
 from shared.core.config import get_settings
 from shared.core.settings.base import BaseAppSettings
+from shared.infrastructure.messaging.rabbitmq_client import (
+    RabbitMQMessagingService,
+)
 from shared.infrastructure.repositories.artifact import ArtifactRepository
+from shared.infrastructure.repositories.document import DocumentRepository
 from shared.infrastructure.repositories.job import JobRepository
 from shared.infrastructure.storage.s3_client import S3StorageService
+from shared.interfaces.infrastructure.messaging import IMessagingService
 from shared.interfaces.infrastructure.storage import IStorageService
 from shared.interfaces.repositories.artifact import IArtifactRepository
+from shared.interfaces.repositories.document import IDocumentRepository
 from shared.interfaces.repositories.job import IJobRepository
 
 from ..infrastructure.parsing.pymupdf_parser import PyMuPDFParser
@@ -47,6 +53,9 @@ class Container:
     def artifact_repository(self) -> IArtifactRepository:
         return ArtifactRepository()
 
+    def document_repository(self) -> IDocumentRepository:
+        return DocumentRepository()
+
     def storage_service(self) -> IStorageService:
         return S3StorageService(
             region=self._settings.aws_region,
@@ -56,9 +65,13 @@ class Container:
     def parser(self) -> IDocumentParser:
         return PyMuPDFParser()
 
+    def messaging_service(self) -> IMessagingService:
+        return RabbitMQMessagingService(url=self._settings.rabbitmq_url)
+
     def processing_service(self) -> ProcessingService:
         return ProcessingService(
             job_repo=self.job_repository(),
+            document_repo=self.document_repository(),
             artifact_repo=self.artifact_repository(),
             storage=self.storage_service(),
             parser=self.parser(),
