d1abf2e7130e5abcf3a63a717b5a34da2de43c0a
Connor R <connorrisse@proton.me>
2026-05-13 18:30:48 +0200

    feat: add structlog and shared logging setup

---
+++ shared/core/logging.py
@@ -0,0 +1,30 @@
+import logging
+
+import structlog
+
+
+def setup_logging(*, json_logs: bool) -> None:
+    shared_processors: list[structlog.types.Processor] = [
+        structlog.contextvars.merge_contextvars,
+        structlog.processors.add_log_level,
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.StackInfoRenderer(),
+    ]
+
+    if json_logs:
+        processors = shared_processors + [
+            structlog.processors.ExceptionRenderer(),
+            structlog.processors.JSONRenderer(),
+        ]
+    else:
+        processors = shared_processors + [
+            structlog.dev.ConsoleRenderer(),
+        ]
+
+    structlog.configure(
+        processors=processors,
+        wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
+        context_class=dict,
+        logger_factory=structlog.PrintLoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
